6fb609854c08e6518c620939712e6f3ad1011a8e
Leo Goldstien <leogoldstien@gmail.com>
2023-06-26 02:59:34 +0300

    Updated deps Removed redundant files

--- tests/test_confluentfucci/test_main.py
+++
@@ -1,9 +0,0 @@
-from __future__ import annotations
-
-from confluentfucci.main import get_hello
-
-
-def it_prints_hi_to_the_project_author() -> None:
-    expected = 'Hello, Leo Goldstien!'
-    actual = get_hello('Leo Goldstien')
-    assert actual == expected
